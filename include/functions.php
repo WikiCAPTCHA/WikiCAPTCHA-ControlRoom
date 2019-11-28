ca91cb2fff63ce54cde862232589249c4e377e9f
Valerio Bozzolan <boz@reyboz.it>
2019-11-28 20:07:21 +0100

    add require_permission() function

--- include/functions.php
+++ include/functions.php
@@ -14,3 +14,24 @@ function template( $name, $args = [] ) {
 
 	require ABSPATH . "/template/$name.php";
 }
+
+/**
+ * Require a specific permission
+ *
+ * @param string $permission
+ */
+function require_permission( $permission ) {
+	if( !has_permission( $permission ) ) {
+
+		if( is_logged() ) {
+			http_response_code( 403 );
+			die( "ARE YOU A PIRATE?" );
+		}
+
+		$url = http_build_get_query( 'login.php', [
+			'redirect' => $_SERVER['REQUEST_URI'],
+		] );
+
+		http_redirect( $url, 303 ); // HTTP 303 redirect: See Other
+	}
+}
