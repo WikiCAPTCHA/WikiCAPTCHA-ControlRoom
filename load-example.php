29e591a992708e260c0a23fec4c1d3de82a63a04
Valerio Bozzolan <gnu@linux.it>
2019-11-30 11:05:37 +0100

    simplify default configuration file

--- load-example.php
+++ load-example.php
@@ -8,29 +8,31 @@
  *******************************************************/
 
 // Database configuration
-$username = 'my.project.user';
-$password = 'super secret password of my.project.user';
-$database = 'myproject';
+$database = 'wikicaptcha';
+$username = 'wikicaptcha';
+$password = 'super secret password of wikicaptcha user';
 $location = 'localhost';
 $charset  = 'utf8mb4';
 
 // Prefix for every your database table (if any)
-//  You can use something as 'foobar_' or an empty string
-$prefix = '';
+//  Ask to Ludovico Pavesi if the API support this as well
+$prefix = 'wcaptcha_';
 
-// How you visit your website?
-//  Use an empty string if you visit it with http://localhost/
-//  Use '/project'      if you visit it with http://localhost/project/
+// How you visit the homepage of this project?
+//  Use '' (empty) if you visit the homepage with http://localhost/
+//  Use '/project' if you visit the homepage with http://localhost/project/
 define( 'ROOT', '' );
 
-// Absolute pathname to this directory
-//  Usually leave it as-is
+// Absolute pathname to the directory containing this configuration file
+// You may leave this set to the magic word __DIR__ that means this directory
 define( 'ABSPATH', __DIR__ );
 
 // Load the framework
 //  You know where the framework is
 //  For example I put it in: '/usr/share/php/suckless-php/load.php'
 //  but often you want to put it in this same directory,
-//  in that case just use this pathname:    'suckless-php/load.php'
+//  in that case just use this pathname:   ABSPATH . '/suckless-php/load.php'
+//  it could also be in the parent dir:    ABSPATH . '/../suckless-php/load.php'
+//  etc.
 //  Anyway, it may be everywhere. You can choose.
-require 'suckless-php/load.php';
+require ABSPATH . '/suckless-php/load.php';
