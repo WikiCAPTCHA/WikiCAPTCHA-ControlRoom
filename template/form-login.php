13376d573a34b6eed18ec3c339cc66deb70a08bf
Valerio Bozzolan <gnu@linux.it>
2019-11-30 11:12:48 +0100

    refactor login page with a dedicated template

--- template/form-login.php
+++ template/form-login.php
@@ -15,24 +15,32 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-// load everything
-require '../load.php';
+/*
+ * This template is the login form
+ */
 
-// this is the homepage (the business logic of this page it's in this class)
-$page = new PageLogin();
-
-// print site header
-$page->printHeader();
 ?>
 
-	<!-- login failed warning -->
-	<?php if( $page->isLoginFormSubmitted() ): ?>
-		<div class="card-panel">
-			<p><?= __( "Login failed!" ) ?></p>
-		</div>
-	<?php endif ?>
+<!-- login form -->
+<form method="post">
 
-	<?php template( 'form-login' ) ?>
+	<?php form_action( 'do-login' ) ?>
 
-<?php
-$page->printFooter();
+	<!-- username -->
+	<p>
+		<label for="user-uid"><?= esc_html( __( "Username" ) ) ?></label>
+		<br />
+		<input type="text" id="user-uid" name="user_uid" />
+	</p>
+
+	<!-- password -->
+	<p>
+		<label for="user-pwd"><?= esc_html( __( "Password" ) ) ?></label>
+		<br />
+		<input type="password" id="user-pwd" name="user_password" />
+	</p>
+
+	<!-- submit button -->
+	<p><button><?= esc_html( __( "Login" ) ) ?></button></p>
+
+</form>
